4a0c94639f63fec6d7ad9ef5691aeb1d2e846eee
molystand <molystand@gmail.com>
2021-04-20 05:04:42 +0300

    базовые eat, pat, beam

--- ai-chan_bot.py
+++ ai-chan_bot.py
@@ -1,3 +1,5 @@
+import os
+import random
 import discord
 from discord.ext import commands
 
@@ -9,11 +11,30 @@ async def beam(ctx):
     await ctx.send('AI-CHAN BEAM!')
 
 
+@bot.command(name='баг')
+async def bugs(ctx, bug_note: str):
+    print(bug_note)
+    await ctx.send('Ваше сообщение о баге передано в службу поддержки! В качестве извинений примите от нас 108 примогемов и крестом по ебалу')
 
 
+@bot.command(aliases=['кушац', 'еда'])
+async def eat(ctx, user: discord.User):
+    author = ctx.author.mention
+    await ctx.send(f'{user.mention}, вас съел {author}')
 
-tokenfile_name = 'token.txt'
-with open(tokenfile_name) as tf:
+
+@bot.command()
+async def pat(ctx, user: discord.User):
+    catalog = './media/gif/pat/'
+    files = os.listdir(catalog)
+    file = random.choice(files)
+    abspath = os.path.abspath(f'{catalog}{file}')
+
+    await ctx.send(f'{user.mention}, вас погладил {ctx.author.mention}', file=discord.File(abspath))
+
+
+token_file_name = 'token.txt'
+with open(token_file_name) as tf:
     bot_token = tf.readline().strip()
 
 bot.run(bot_token)
